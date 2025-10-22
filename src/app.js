deb68518fefc8c89492dd518f86c78119770099f
nkeerthireddy15 <nkeerthireddy15@gmail.com>
2025-10-22 23:32:53 +0530

    22-oct-middlewares

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/hello",
-  (req, res, next) => {
-    console.log("one");
-    res.send("hellookeerthi!!!!");
+app.use("/admin", (req, res, next) => {
+  const token = "xyz";
+  const isauthorised = token === "xyz";
+  if (isauthorised) {
     next();
-  },
-  (req, res) => {
-    console.log("two");
-    res.send("hello response2");
+  } else {
+    res.status(401).send("admin not authorised");
   }
-);
+});
+
+app.get("/admin/user", (req, res, next) => {
+  res.send("hello response");
+});
+
+app.get("/admin/delete", (req, res, next) => {
+  res.send("hello response delete");
+});
 
 app.listen(7777, () => {
   console.log("app is listening at 77777");
